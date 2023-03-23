e30f31faeead0330df70e79e1bc5b9716782fabc
NilaAyu1611 <nilaayucw@gmail.com>
2023-03-23 21:45:59 +0700

    membuat inputan dan memanggil variabel

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -7,3 +7,6 @@ int n;
 void input()
 {   // Procedure Input
     while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";   // Mmebuaut inputan jumlah elemnet Array
+        cin >> n;                                       // Memanggil variabel inputan n
